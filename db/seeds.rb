16c2d632aaa74c0f8770ca5b5f543392a62501ff
Mnkith <munkithabid@gmail.com>
2021-01-30 13:55:10 -0600

    work on cause's images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,18 @@
-c1 = Cause.new title: 'c1'
-c1.save 
+desc = "Welcome to the Sinatra Template! If you're seeing this page, then everything is working
+as expected. To get started, delete this file (<code>app/views/welcome.erb)</code> and begin adding
+your own actions to <code>application_controller.rb</code>."
 
-puts c1.errors.to_a
+
+
+
+Cause.destroy_all
+
+c1 = Cause.create title: 'c1', description: desc, image_link: 'im1.gif'
+c2 = Cause.create title: 'c2', description: desc, image_link: 'im2.gif'
+c3 = Cause.create title: 'c3', description: desc, image_link: 'im3.gif'
+c4 = Cause.create title: 'c4', description: desc, image_link: 'im4.gif'
+c5 = Cause.create title: 'c5', description: desc, image_link: 'im5.gif'
+# c1.save 
+
+# puts c1.errors.to_a
 # c1.save validate: false
-# Cause.destroy_all
