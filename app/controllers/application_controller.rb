5f0358869799c698bf5747f2047c2594ae220aa5
Mnkith <munkithabid@gmail.com>
2021-01-31 01:23:34 -0600

    activate sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < Sinatra::Base
   # register Sinatra::Twitter::Bootstrap::Assets
 
   configure do
+    enable :sessions
+    set :session_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
