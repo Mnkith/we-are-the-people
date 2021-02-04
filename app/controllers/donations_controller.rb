860c7370bd8cb7b67277d9ad2ee12afe88c74436
Mnkith <munkithabid@gmail.com>
2021-02-04 14:27:36 -0600

    scaffold the donation MVC

---
+++ app/controllers/donations_controller.rb
@@ -0,0 +1,37 @@
+class DonationsController < ApplicationController
+
+  # GET: /donations
+  get "/donations" do
+    erb :"/donations/index.html"
+  end
+
+  # GET: /donations/new
+  get "/donations/new" do
+    erb :"/donations/new.html"
+  end
+
+  # POST: /donations
+  post "/donations" do
+    redirect "/donations"
+  end
+
+  # GET: /donations/5
+  get "/donations/:id" do
+    erb :"/donations/show.html"
+  end
+
+  # GET: /donations/5/edit
+  get "/donations/:id/edit" do
+    erb :"/donations/edit.html"
+  end
+
+  # PATCH: /donations/5
+  patch "/donations/:id" do
+    redirect "/donations/:id"
+  end
+
+  # DELETE: /donations/5/delete
+  delete "/donations/:id/delete" do
+    redirect "/donations"
+  end
+end
