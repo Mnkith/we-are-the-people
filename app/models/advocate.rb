02d77869c97c43e7fa104755d439ba74fde8cb13
Mnkith <munkithabid@gmail.com>
2021-02-04 14:38:21 -0600

    build the advocate associations

--- app/models/advocate.rb
+++ app/models/advocate.rb
@@ -1,2 +1,7 @@
 class Advocate < ActiveRecord::Base
+  extend Slug::ClassMethods
+  include Slug::InstanceMethods
+
+  has_many :donations
+  has_many :causes, through: :donations
 end
--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,2 +1,4 @@
 class Donation < ActiveRecord::Base
+  extend Slug::ClassMethods
+  include Slug::InstanceMethods
 end
