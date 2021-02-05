8ed6e80f52ee4f631b2810aebf76b735cebc58d5
Mnkith <munkithabid@gmail.com>
2021-02-05 12:55:57 -0600

    create login controller/rout

--- app/controllers/advocates_controller.rb
+++ app/controllers/advocates_controller.rb
@@ -34,6 +34,15 @@ class AdvocatesController < ApplicationController
     end
   end
   
+  post '/login' do
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/users/home'
+    end
+    redirect '/sessions/login'
+  end
+
   get "/advocates/registration" do
     if session[:id]
       session.clear
