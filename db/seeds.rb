d904f3ae7915e03cc7460763fbb643225a637a7e
Mnkith <munkithabid@gmail.com>
2021-02-13 20:49:09 -0600

    fix donation bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@ your own actions to <code>application_controller.rb</code>."
 
 Cause.destroy_all
 
+
 c1 = Cause.create name: 'Plant For The Planet', description: desc, image_link: 'images/im1.gif'
 c2 = Cause.create name: 'c2', description: desc, image_link: 'images/im2.gif'
 c3 = Cause.create name: 'c3', description: desc, image_link: 'images/im3.gif'
