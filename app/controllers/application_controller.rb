2b9800164a0d3509781a693ab78bdb73414db4b5
Mnkith <munkithabid@gmail.com>
2021-02-13 21:39:14 -0600

    create #admin? advocate instance method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :home
+    erb :'/causes/index'
   end
 
   helpers do
--- app/models/advocate.rb
+++ app/models/advocate.rb
@@ -13,4 +13,8 @@ class Advocate < ActiveRecord::Base
 
   has_many :donations
   has_many :causes, through: :donations
+
+  def admin?
+    self.name == 'admin'
+  end
 end
