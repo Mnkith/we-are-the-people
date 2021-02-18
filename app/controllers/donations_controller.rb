febe2eb4744fd27656313ed25acdb16679695e7d
Mnkith <munkithabid@gmail.com>
2021-02-17 22:53:49 -0600

    create #check_met callback at before_save cause

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -5,44 +5,43 @@ class DonationsController < ApplicationController
     erb :"/donations/index.html"
   end
 
-  # GET: /donations/new
+  
   get "/donations/new" do
-    # binding.pry
     erb :"/donations/new.html"
   end
 
   # POST: /donations
   post "/donations" do
-    @advocate = current_user
+    advocate = current_user
     #prefer different amount over preset amount
-    @donation = !params[:different].empty? ? Donation.create(amount: params[:different]) : Donation.create(amount: params[:preset])
-    if @donation.valid?
-      @donation.advocate = @advocate  
+    donation = !params[:different].empty? ? Donation.create(amount: params[:different]) : Donation.create(amount: params[:preset])
+    if donation.valid?
+      donation.advocate = advocate  
       cause = Cause.find session[:cause_id]
-      cause.current += @donation.amount
-      if cause.current >= cause.goal 
-        cause.met = true
-        cause.current = cause.goal 
-      end
-      # @donation.advocate_name = @advocate.name
-      # @donation.cause_name = cause.name
+      donation.cause = cause
+      cause.current += donation.amount
+      # if cause.current >= cause.goal 
+      #   cause.met = true
+      #   cause.current = cause.goal 
+      # end
+      # donation.advocate_name = advocate.name
+      # donation.cause_name = cause.name
       # binding.pry
-      @donation.cause = cause
-      # cause.donations << @donation
-      # cause.save
-      # @advocate.donations << @donation
-      # @advocate.save
-      @donation.save
+      # cause.donations << donation
+      cause.save
+      # advocate.donations << donation
+      # advocate.save
+      donation.save
 
-      redirect to "/donations/#{@donation.id}"
+      redirect to "/donations/#{donation.id}"
     else
        erb :"/donations/new.html"
     end
   end
 
-  # GET: /donations/5
   get "/donations/:id" do
     @donation = Donation.find params[:id]
     erb :"/donations/show.html"
   end
+  
 end
--- app/models/cause.rb
+++ app/models/cause.rb
@@ -5,6 +5,19 @@ class Cause < ActiveRecord::Base
   validates :name, uniqueness: true, presence: true
   validates :goal, presence: true, numericality: { only_integer: true, greater_than: 0}
 
+  before_save :check_met
+
+  private 
+
+    def check_met
+      if self.current >= self.goal 
+        self.met = true
+        self.current = self.goal 
+      end
+    end
+
+
+
   has_many :donations, primary_key: "name", foreign_key: "cause_name"
   has_many :advocates, through: :donations
 end
